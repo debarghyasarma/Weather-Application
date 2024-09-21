cf70bb64b3c3b8f410b923c4b714d28763845a10
Debarghya Sharma <68813537+debarghyasarma@users.noreply.github.com>
2024-09-21 16:28:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ function App() {
                 </p>
               ) : null}
 
-              <p>Feels Like Temp</p>
+              <p>Feels Like</p>
             </div>
             <div className="humidity">
               {data.main ? <p className="bold">{data.main.humidity}%</p> : null}

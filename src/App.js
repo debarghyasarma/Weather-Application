413c3d6083fbfe524fa0eb6bd73fe03830477f23
Debarghya Sharma <68813537+debarghyasarma@users.noreply.github.com>
2024-09-22 21:17:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
           type="text"
           value={location}
           onChange={(event) => setLocation(event.target.value)}
-          placeholder="Enter Locations"
+          placeholder="Enter Places"
           onKeyPress={searchLocation}
         />
       </div>
